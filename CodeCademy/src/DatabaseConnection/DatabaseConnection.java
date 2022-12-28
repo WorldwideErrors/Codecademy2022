caa04fc2c664853ad2946a54554c458779ac9fe9
J.Seremak <janko2002@outlook.com>
2022-12-28 11:20:38 +0100

    DatabaseConnection class big changes
    Added a method for getConnection() and made class considerably smaller.

--- CodeCademy/src/DatabaseConnection/DatabaseConnection.java
+++ CodeCademy/src/DatabaseConnection/DatabaseConnection.java
@@ -3,66 +3,61 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-    public static void main(String[] args) {
+    private static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static final String URL = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=CodeCademyRE;user=janko;password=Morgen;encrypt=true;trustServerCertificate=true";
 
-        // Dit zijn de instellingen voor de verbinding. Vervang de databaseName indien deze voor jou anders is.
-        String connectionUrl = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=CodeCademyRE;user=janko;password=Morgen;encrypt=true;trustServerCertificate=true";
-        // Connection beheert informatie over de connectie met de database.
-        Connection con = null;
-
-        // Statement zorgt dat we een SQL query kunnen uitvoeren.
-        Statement stmt = null;
-
-        // ResultSet is de tabel die we van de database terugkrijgen.
-        // We kunnen door de rows heen stappen en iedere kolom lezen.
-        ResultSet rs = null;
+    public static Connection getConnection() {
         try {
-            // 'Importeer' de driver die je gedownload hebt.
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            // Maak de verbinding met de database.
-            con = DriverManager.getConnection(connectionUrl);
-
-            // Stel een SQL query samen.
-            //String SQL = "SELECT TOP 10 * FROM Employee";
-            
-            String SQL = "INSERT INTO Employee(Name, Email) Values('test','test@gmail.com')";
-            stmt = con.createStatement();
-            // Voer de query uit op de database.
-            rs = stmt.executeQuery(SQL);
-
-            System.out.print(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
-
-            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
-            while (rs.next()) {
-                // Vraag per row de kolommen in die row op.
-                String Name = rs.getString("Name");
-                String Email = rs.getString("Email");
-
-                // Print de kolomwaarden.
-                // System.out.println(ISBN + " " + title + " " + author);
-                // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
-                // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
-                //    System.out.format("| %7d | %-32s | %-24s | \n", Name, Email);   DIDNT WORK, LOOK AT LATER
-                System.out.println(Name + " " + Email);
-            }
-            System.out.println(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
-
-        } // Handle any errors that may have occurred.
-        catch (Exception e) {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL);
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (rs != null) try {
-                rs.close();
-            } catch (Exception e) {
-            }
-            if (stmt != null) try {
-                stmt.close();
-            } catch (Exception e) {
-            }
-            if (con != null) try {
-                con.close();
-            } catch (Exception e) {
-            }
+            return null;
         }
     }
+
+//    public static Connection getConnection() {
+//
+//        String connectionUrl = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=CodeCademyRE;user=janko;password=Morgen;encrypt=true;trustServerCertificate=true";
+//
+//        Connection con = null;
+//        Statement stmt = null;
+//
+//        ResultSet rs = null;
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            con = DriverManager.getConnection(connectionUrl);
+//            return con;
+//            String SQL = "INSERT INTO Employee(Name, Email) Values('test','test@gmail.com')";
+//            stmt = con.createStatement();
+//            rs = stmt.executeQuery(SQL);
+//
+//            System.out.print(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
+//
+//            while (rs.next()) {
+//                String Name = rs.getString("Name");
+//                String Email = rs.getString("Email");
+//
+//                System.out.println(Name + " " + Email);
+//            }
+//            System.out.println(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        } finally {
+//            if (rs != null) try {
+//                rs.close();
+//            } catch (Exception e) {
+//            }
+//            if (stmt != null) try {
+//                stmt.close();
+//            } catch (Exception e) {
+//            }
+//            if (con != null) try {
+//                con.close();
+//            } catch (Exception e) {
+//            }
+//        }
+//    }
 }
